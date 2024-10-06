cf0d47db749e73f1181b2c890f8a87dd6b5a8a51
Marcin <242138+mrcne@users.noreply.github.com>
2024-10-06 13:15:29 +0200

    Zoom in by opening modal for event charts

--- scamp_app/src/components/PanelSide/index.tsx
+++ scamp_app/src/components/PanelSide/index.tsx
@@ -1,4 +1,14 @@
-import {Box, BoxProps, Heading, Image} from "@chakra-ui/react";
+import {
+  Box,
+  BoxProps,
+  Button, Flex,
+  Heading,
+  Image,
+  Modal, ModalBody, ModalCloseButton,
+  ModalContent, ModalFooter, ModalHeader,
+  ModalOverlay,
+  useDisclosure
+} from "@chakra-ui/react";
 import {useCallback} from "react";
 
 import {useSeismicSettings} from "../../context/seismicSettings.tsx";
@@ -6,6 +16,7 @@ import {SeismicSettings} from "../../types/seismicSettings.ts";
 
 const PanelSide: React.FC<BoxProps> = (props) => {
   const { settings } = useSeismicSettings();
+  const { isOpen, onOpen, onClose } = useDisclosure()
 
   const filePath = useCallback((event: SeismicSettings['event'], dataSet: SeismicSettings['dataSet']) => {
     return `./data/${dataSet}/training/plots/${event}.png`;
@@ -28,12 +39,44 @@ const PanelSide: React.FC<BoxProps> = (props) => {
   >
     <Heading as="h2" size="sm">{settings.event}</Heading>
     Confidence: 0.8
+
     <br />
     <Image
       maxWidth="100%"
       src={filePath(settings.event, settings.dataSet)}
       alt={`${settings.event} seismic event`}
+      onClick={onOpen}
+      cursor="zoom-in"
     />
+    <Flex>
+      <Button onClick={onOpen} margin="auto" cursor="pointer">Magnify</Button>
+    </Flex>
+
+    <Modal
+      isOpen={isOpen}
+      onClose={onClose}
+      size="full"
+    >
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader textAlign="center">{`${settings.event} seismic event`}</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody justifyContent="center" display="flex" textAlign="center">
+          <Image
+            src={filePath(settings.event, settings.dataSet)}
+            alt={`${settings.event} seismic event`}
+          />
+        </ModalBody>
+
+        <ModalFooter justifyContent="center">
+          <Button variant='ghost'>Previous event</Button>
+          <Button colorScheme='blue' mr={3} onClick={onClose}>
+            Close
+          </Button>
+          <Button variant='ghost'>Next event</Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
   </Box>
 };
 
